8e59685e1d5b42966eded2951784e9e3777fec0a
Casneil Simpson <casneil.simpson@gmail.com>
2019-10-20 06:40:38 +0200

    fixed errors

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import React from "react";
 // import { Route, BrowserRouter, Switch } from "react-router-dom";
 
@@ -18,24 +17,3 @@
 // };
 
 // export default Router;
-=======
-// import React from "react";
-// import { Route, BrowserRouter, Switch } from "react-router-dom";
-
-// import LinkButton from "./LinkButton";
-// import App from "../App";
-
-// const Router = () => {
-//   return (
-//     <BrowserRouter>
-//       <Switch>
-//         <Route exact path="/" component={App} />
-
-//         <Route exact path="/http://:uri" component={LinkButton} />
-//       </Switch>
-//     </BrowserRouter>
-//   );
-// };
-
-// export default Router;
->>>>>>> 57939b8e752f153a44a9eda3a0dc87f6dd8bb60f
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import React, { useState } from "react";
 // import { makeStyles, MuiThemeProvider } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
@@ -65,71 +64,3 @@
 // };
 
 // export default Search;
-=======
-// import React, { useState } from "react";
-// import { makeStyles, MuiThemeProvider } from "@material-ui/core/styles";
-// import Button from "@material-ui/core/Button";
-// import TextField from "@material-ui/core/TextField";
-// import { createMuiTheme } from "@material-ui/core/styles";
-
-// const useStyles = makeStyles(theme => ({
-//   container: {
-//     display: "flex",
-//     flexWrap: "wrap"
-//   },
-
-//   button: {
-//     margin: theme.spacing(1)
-//   },
-//   textField: {
-//     marginLeft: theme.spacing(1),
-//     marginRight: theme.spacing(1),
-//     width: 200
-//   }
-// }));
-
-// const theme = createMuiTheme({
-//   palette: {
-//     primary: {
-//       main: "#ff6d00",
-//       contrastText: "#fff"
-//     },
-//     secondary: {
-//       main: "#00e676",
-//       contrastText: "#fff"
-//     }
-//   }
-// });
-
-// const Search = props => {
-//   const classes = useStyles();
-//   return (
-//     <MuiThemeProvider theme={theme}>
-//       <div>
-//         <form onSubmit={props.submit}>
-//           <TextField
-//             onSubmit={props.search}
-//             id="standard-with-placeholder"
-//             label=""
-//             placeholder="Search Recipes"
-//             className={classes.textField}
-//             margin="normal"
-//             onChange={props.search}
-//           />
-
-//           <Button
-//             variant="contained"
-//             color="primary"
-//             className={classes.button}
-//             onClick={props.submit}
-//           >
-//             Search
-//           </Button>
-//         </form>
-//       </div>
-//     </MuiThemeProvider>
-//   );
-// };
-
-// export default Search;
->>>>>>> 57939b8e752f153a44a9eda3a0dc87f6dd8bb60f
