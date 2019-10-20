212a1f08395d9fd585bb8ef29ce6f12ce48c16fa
Casneil Simpson <casneil.simpson@gmail.com>
2019-10-20 06:42:42 +0200

    Updates

---
+++ precache-manifest.28ff2784c737a41147b74d58cf70bf6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20913f34435946d6b91cfdc1f582298a",
+    "url": "/RecipeFetcher/index.html"
+  },
+  {
+    "revision": "0c08581a60689593018e",
+    "url": "/RecipeFetcher/static/css/main.2da19fae.chunk.css"
+  },
+  {
+    "revision": "58d94a45d7a009b83bad",
+    "url": "/RecipeFetcher/static/js/2.3176286a.chunk.js"
+  },
+  {
+    "revision": "0c08581a60689593018e",
+    "url": "/RecipeFetcher/static/js/main.ae1d5c27.chunk.js"
+  },
+  {
+    "revision": "95c5d6e22fe96b970449",
+    "url": "/RecipeFetcher/static/js/runtime-main.449e40dc.js"
+  }
+]);
